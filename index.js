a3741bb7707a35d405622a29e6266557c1bbc342
Yaser-2004 <133482087+Yaser-2004@users.noreply.github.com>
2024-10-01 20:13:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+alert("Please hover over to see the beautifull effect");
+//top1
+document.querySelector(".top1").addEventListener("mouseover", () => {
+    document.querySelector("#top1").style.height = "100%";
+})
+
+document.querySelector(".top1").addEventListener("mouseout", () => {
+    document.querySelector("#top1").style.height = "0";
+})
+
+//top21
+document.querySelector(".top21").addEventListener("mouseover", () => {
+    document.querySelector("#top21").style.width = "100%";
+})
+
+document.querySelector(".top21").addEventListener("mouseout", () => {
+    document.querySelector("#top21").style.width = "0";
+})
+
+//top22
+document.querySelector(".top22").addEventListener("mouseover", () => {
+    document.querySelector("#top22").style.width = "100%";
+})
+
+document.querySelector(".top22").addEventListener("mouseout", () => {
+    document.querySelector("#top22").style.width = "0";
+})
+
+//top3
+document.querySelector(".top3").addEventListener("mouseover", () => {
+    document.querySelector("#top3").style.height = "100%";
+})
+
+document.querySelector(".top3").addEventListener("mouseout", () => {
+    document.querySelector("#top3").style.height = "0";
+})
+
+//down1
+document.querySelector(".down1").addEventListener("mouseover", () => {
+    document.querySelector("#down1").style.width = "100%";
+})
+
+document.querySelector(".down1").addEventListener("mouseout", () => {
+    document.querySelector("#down1").style.width = "0";
+})
+
+//down2
+document.querySelector(".down2").addEventListener("mouseover", () => {
+    document.querySelector("#down2").style.width = "100%";
+})
+
+document.querySelector(".down2").addEventListener("mouseout", () => {
+    document.querySelector("#down2").style.width = "0";
+})
